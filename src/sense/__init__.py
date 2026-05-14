061272f48345c0ce42b74b5d64abd9453b7a40c4
maya-fakih <alfakihmaya1@gmail.com>
2026-05-14 16:48:06 +0300

    fixed imports in src/see/__init__

--- src/sense/__init__.py
+++ src/sense/__init__.py
@@ -1,4 +1,6 @@
 from sense.sensors.sensor_base import Sensor
 from sense.sensor_fuser import SensorFuser
 from sense.sensor_parser import SensorParser
-from sense.snapshot import SensorSnapshot
+from see.snapshot import VisionSnapshot
+from see.models.detection import Detection
+from see.models.fire_cluster import FireCluster
