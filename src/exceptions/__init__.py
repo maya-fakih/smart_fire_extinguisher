141b23d7dc088a5cd00eb98fa5707e24b301fb85
maya-fakih <alfakihmaya1@gmail.com>
2026-05-14 16:48:53 +0300

    fixed imports in src/exceptions/__init__ to match the actual error calls

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,8 +1,12 @@
 from exceptions.exceptions import (
     ConfigError,
     StateInitError,
+    LayerFaultError,
+    SensorFaultError,
+    ActuatorFaultError,
     ModeError,
     DatabaseError,
     AlignmentError,
     ModelError,
+    IKError,
 )
